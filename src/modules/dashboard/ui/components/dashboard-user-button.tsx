92108b6731099a61ce1cea82e2091d4739ba61e9
pavan kumar <rajulapatipavankr@gmail.com>
2025-07-20 11:34:52 +0530

    ui changes

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,58 +1,108 @@
 import { authClient } from "@/lib/auth-client"
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger
+} from "@/components/ui/drawer"
+
 import { Avatar } from "@/components/ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
 import { GeneratedAvtar } from "@/components/generated-avathar";
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 export const DashboardUserButton = () => {
     const router = useRouter()
-    const { data, isPending} = authClient.useSession();
+    const isMobile = useIsMobile()
+    const { data, isPending } = authClient.useSession();
 
-    if(isPending || !data?.user) {
+    if (isPending || !data?.user) {
         return null
     }
 
-    const onLogout = async() => {
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-start bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
+                    {data.user.image ? (
+                        <Avatar>
+                            <AvatarImage src={data.user.image} />
+                        </Avatar>
+                    ) : <GeneratedAvtar
+                        seed={data.user.name}
+                        variant="botttsNeutral"
+                        className="size-9 mr-3"
+                    />}
+                    <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                        <p className="text-sm truncate w-full">
+                            {data.user.name}
+                        </p>
+                        <p className="text-xs truncate w-full">
+                            {data.user.email}
+                        </p>
+                    </div>
+                    <ChevronDownIcon className="size-4 shrink-0" />
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{data.user.name}</DrawerTitle>
+                        <DrawerDescription>{data.user.email}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button variant="outline" onClick={() => {}}><CreditCardIcon className="size-4 text-black"/>Billing</Button>
+                        <Button variant="outline" onClick={() => {onLogout}}><LogOutIcon className="size-4 text-black"/>Logout</Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        )
+    }
+
+    const onLogout = async () => {
         await authClient.signOut({
-            fetchOptions : {
-                onSuccess : () => {
+            fetchOptions: {
+                onSuccess: () => {
                     router.push("/auth/sign-in")
                 }
             }
         })
     }
-    
+
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-start bg-white/5 hover:bg-white/10 overflow-hidden">
-            {data.user.image ? (
-                <Avatar>
-                    <AvatarImage src={data.user.image}/>
-                </Avatar>
-            ) : <GeneratedAvtar 
-            seed={data.user.name}
-            variant="botttsNeutral"
-            className="size-9 mr-3"
-            />}
-            <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
-                <p className="text-sm truncate w-full">
-                    {data.user.name}
-                </p>
-                <p className="text-xs truncate w-full">
-                    {data.user.email}
-                </p>
-            </div>
-            <ChevronDownIcon className="size-4 shrink-0"/>
+            <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-start bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
+                {data.user.image ? (
+                    <Avatar>
+                        <AvatarImage src={data.user.image} />
+                    </Avatar>
+                ) : <GeneratedAvtar
+                    seed={data.user.name}
+                    variant="botttsNeutral"
+                    className="size-9 mr-3"
+                />}
+                <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                    <p className="text-sm truncate w-full">
+                        {data.user.name}
+                    </p>
+                    <p className="text-xs truncate w-full">
+                        {data.user.email}
+                    </p>
+                </div>
+                <ChevronDownIcon className="size-4 shrink-0" />
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" side="right" className="w-72">
                 <DropdownMenuLabel>
@@ -61,14 +111,14 @@ export const DashboardUserButton = () => {
                         <span className="text-sm font-normal text-muted-foreground truncate">{data.user.email}</span>
                     </div>
                 </DropdownMenuLabel>
-                <DropdownMenuSeparator/>
+                <DropdownMenuSeparator />
                 <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
                     Billing
-                    <CreditCardIcon className="size-4"/>
+                    <CreditCardIcon className="size-4" />
                 </DropdownMenuItem>
                 <DropdownMenuItem className="cursor-pointer flex items-center justify-between" onClick={onLogout}>
                     Logout
-                    <LogOutIcon className="size-4"/>
+                    <LogOutIcon className="size-4" />
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
