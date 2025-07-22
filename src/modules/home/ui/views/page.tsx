2efab9d9ab99aa0d67f47d76a5311d6d95e142b3
pavan kumar <rajulapatipavankr@gmail.com>
2025-07-22 22:55:08 +0530

    Agents setup

--- src/modules/home/ui/views/page.tsx
+++ src/modules/home/ui/views/page.tsx
@@ -1,15 +1,9 @@
 "use client"
 
-import { useTRPC } from "@/trpc/client"
-import { useQuery } from "@tanstack/react-query"
 
 export const HomeView = () => {
-  const trpc = useTRPC()
-  const { data, isLoading } = useQuery(
-    trpc.hello.queryOptions({ text: "Pavan kumar" })
-  )
 
-  if (isLoading) return <p>Loading...</p>
-
-  return <div>{data?.greeting}</div>
+  return <div>
+    Home view
+  </div>
 }
