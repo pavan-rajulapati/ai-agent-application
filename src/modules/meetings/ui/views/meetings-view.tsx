3a20e016b70641b319680b2334eced6f5f099c4a
pavan kumar <rajulapatipavankr@gmail.com>
2025-07-27 23:46:56 +0530

    implemented meetings setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { ErrorState } from "@/components/error-state copy"
+import { LoadingState } from "@/components/loading-state"
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query"
+
+export const MeetingsView = () => {
+    const trpc = useTRPC()
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    )
+}
+
+export const MeetingsViewLoading = () => {
+    return (
+        <LoadingState title="Loading Meetings" description="This may take a few seconds"/>
+    )
+}
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState title="Error occured" description="Something wrong with the meetings"/>
+    )
+}
