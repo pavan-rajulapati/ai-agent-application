4434813fb57b76106497c16f00a1bb5809cdfde4
pavan kumar <rajulapatipavankr@gmail.com>
2025-07-31 07:35:53 +0530

    implemented meetings data table

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,29 +1,38 @@
 "use client"
 
+import { DataTable } from "@/components/data-table"
 import { ErrorState } from "@/components/error-state copy"
 import { LoadingState } from "@/components/loading-state"
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query"
+import { columns } from "../components/columns"
+import { EmptyState } from "@/components/empty-state"
 
 export const MeetingsView = () => {
     const trpc = useTRPC()
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
 
     return (
-        <div className="overflow-x-scroll">
-            {/* {JSON.stringify(data)} */}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex-col gap-y-4">
+            <DataTable data={data.items} columns={columns} />
+            {data.items.length === 0 && (
+                <EmptyState
+                    title="Create your first meeting"
+                    description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants in real time"
+                />
+            )}
         </div>
     )
 }
 
 export const MeetingsViewLoading = () => {
     return (
-        <LoadingState title="Loading Meetings" description="This may take a few seconds"/>
+        <LoadingState title="Loading Meetings" description="This may take a few seconds" />
     )
 }
 
 export const MeetingsViewError = () => {
     return (
-        <ErrorState title="Error occured" description="Something wrong with the meetings"/>
+        <ErrorState title="Error occured" description="Something wrong with the meetings" />
     )
 }
