f45133075412876a96388841497bb33e9e46e8d1
pavan kumar <rajulapatipavankr@gmail.com>
2025-08-03 16:04:39 +0530

    implemented meetings filters

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { AgentsSearchFilter } from "./agents-search-filter";
 import { DEFAULT_PAGE } from "@/constants";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export const AgentsListHeaders = () => {
     const [filters, setFilters] = useAgentsFilters();
@@ -23,25 +24,28 @@ export const AgentsListHeaders = () => {
 
     return (
         <>
-        <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}/>
-        <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <div className="flex items-center justify-between">
-                <h5 className="font-medium text-xl">My Agents</h5>
-                <Button onClick={ () => setIsDialogOpen(true)}>
-                    <PlusIcon/>
-                    New Agent
-                </Button>
-            </div>
-            <div className="flex items-center gap-x-2 p-1">
-                <AgentsSearchFilter/>
-                {isAnyFilterModified && (
-                    <Button variant="outline" size="sm" onClick={onClearFilters}>
-                        <XCircleIcon/>
-                        Clear
+            <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
+            <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <div className="flex items-center justify-between">
+                    <h5 className="font-medium text-xl">My Agents</h5>
+                    <Button onClick={() => setIsDialogOpen(true)}>
+                        <PlusIcon />
+                        New Agent
                     </Button>
-                )}
+                </div>
+                <ScrollArea>
+                    <div className="flex items-center gap-x-2 p-1">
+                        <AgentsSearchFilter />
+                        {isAnyFilterModified && (
+                            <Button variant="outline" size="sm" onClick={onClearFilters}>
+                                <XCircleIcon />
+                                Clear
+                            </Button>
+                        )}
+                    </div>
+                    <ScrollBar orientation="horizontal" />
+                </ScrollArea>
             </div>
-        </div>
         </>
     )
 }
