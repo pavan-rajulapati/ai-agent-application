0036bece2ad7590c253bfc60813b3f0e141c4ebc
pavan kumar <rajulapatipavankr@gmail.com>
2025-08-04 16:54:06 +0530

    implemented meeting variants

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -9,6 +9,10 @@ import { useRouter } from "next/navigation";
 import { useConfirm } from "@/hooks/use-confirm";
 import { useState } from "react";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface MeetingIdViewProps {
     meetingId: string;
@@ -44,6 +48,12 @@ export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
         await removeMeeting.mutateAsync({ id: meetingId });
     };
 
+    const isActive = data.status === "active";
+    const isUpcoming = data.status === "upcoming";
+    const isCancelled = data.status === "cancelled";
+    const isCompleted = data.status === "completed";
+    const isProcessing = data.status === "processing";
+
     return (
         <>
             <RemoveConfirmation />
@@ -56,10 +66,24 @@ export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
                 <MeetingIdViewHeader
                     meetingId={meetingId}
                     meetingName={data.name}
-                    onEdit={() => setUpdateMeetingDialogOpen(true)} // ✅ Correct usage
+                    onEdit={() => setUpdateMeetingDialogOpen(true)}
                     onRemove={handleRemoveMeeting}
                 />
-                <>{JSON.stringify(data, null, 2)}</>
+                {isActive && (
+                    <ActiveState
+                        meetingId={meetingId}
+                    />
+                )}
+                {isCancelled && <CancelledState/>}
+                {isProcessing && <ProcessingState/>}
+                {isCompleted && <div>Completed</div>}
+                {isUpcoming && (
+                    <UpcomingState
+                        meetingId={meetingId}
+                        onCancelMeeting={() => {}}
+                        isCancelling={false}
+                    />
+                )}
             </div>
         </>
     );
